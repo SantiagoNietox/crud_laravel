7255a893f2943cf5b1eb0518511c0d525a240b5a
Santiago Nieto <116695094+SantiagoNietox@users.noreply.github.com>
2023-10-23 15:00:04 -0500

    cambios

--- resources/views/edituser.blade.php
+++ resources/views/edituser.blade.php
@@ -2,12 +2,14 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Registro</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <body>
-    
+
 
     <div class="container">
         <div class="row justify-content-center">
@@ -15,7 +17,7 @@
                 <div class="card">
                     <div class="card-header">Editar usuario</div>
                     <div class="card-body">
-                    <form action="{{ route('viewusers.update', ['viewuser' => $users->id]) }}" method="POST">
+                        <form action="{{ route('viewusers.update', ['viewuser' => $users->id]) }}" method="POST">
                             @method('PUT')
                             @csrf
                             <div class="form-group">
@@ -39,17 +41,17 @@
                                 <label for="cantidad">sexo:</label>
                                 <input type="text" class="form-control" name="sexo" value="{{$users->sexo}}" required>
                             </div>
-                            <div class="form-group" id= "divs">
+                            <div class="form-group" id="divs">
                                 <label for="cantidad">Direccion:</label>
                                 <input type="text" class="form-control" name="address" value="{{$users->address}}" required>
                             </div>
 
 
-    
+
                             <div class="form-group text-center" style="margin-top: 20px;">
                                 <button type="submit" class="btn btn-primary" style=" border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Editar</button>
                             </div>
-                            
+
                         </form>
                     </div>
                 </div>
